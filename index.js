d8b0ebfbf7ecf1aee22d0aed462707a108d9a099
Kelsey Fecho <kfecho30@gmail.com>
2018-09-08 01:15:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // Code your solution here!
 
+function printString(myString) {
+  console.log(myString[0])
+
+  if(myString.length > 1) {
+    let sub = myString.substring(1);
+    printString(sub)
+  } else {
+    return true;
+  }
+}
